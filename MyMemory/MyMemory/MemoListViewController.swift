39585c1df5ad7dfb23d2b1c34a354fb329e98bb8
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 02:10:57 +0900

    add : 목록 뷰 아울렛 변수와 메소드 생성 및 연결

--- MyMemory/MyMemory/MemoListViewController.swift
+++ MyMemory/MyMemory/MemoListViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class MemoListViewController: UIViewController {
-
+class MemoListViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: 테이블 구성 메소드들
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableview(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        
+    }
+    
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // MARK: ViewWillAppear
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
 
 }
 
