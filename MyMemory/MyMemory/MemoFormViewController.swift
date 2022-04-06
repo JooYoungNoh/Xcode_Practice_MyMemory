ed93c3440309beb86a453baabf72e1b30430e896
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 01:53:42 +0900

    add : 메모를 저장하기 위한 코드 작성
    UITextViewDelegate를 추가하고 저장 버튼을 눌렀을 때 AppDelegate에 있는 memolist 배열에 값들을 저장

--- MyMemory/MyMemory/MemoFormViewController.swift
+++ MyMemory/MyMemory/MemoFormViewController.swift
@@ -7,15 +7,49 @@
 
 import UIKit
 
-class MemoFormViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemoFormViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
 
     var subject: String!        //제목을 저장할 객체
     
     @IBOutlet weak var contentsView: UITextView!
     @IBOutlet weak var preview: UIImageView!
     
+    // MARK: 저장 버튼을 클릭했을 때 호출되는 메소드
     @IBAction func save(_ sender: UIBarButtonItem){
+        //내용을 입력하지 않는 경우 경고
+        guard self.contentsView.text?.isEmpty == false else {
+            let alert = UIAlertController(title: nil, message: "내용을 입력해주세요", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: false)
+            return
+        }
+        //MemoData 객체를 생성하고, 데이터를 담는다.
+        let data = MemoData()
         
+        data.title = self.subject                   //제목
+        data.contents = self.contentsView.text      //내용
+        data.image = self.preview.image             //이미지
+        data.regdate = Date()                       //작성 시각
+        
+        //앱 델리게이트 객체를 읽어온 다음, memolist 배열에 MemoData 객체를 추가
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memolist.append(data)
+        
+        //작성폼 화면을 종료하고 이전 화면으로 되돌아간다.
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    //사용자가 텍스트 뷰에 뭔가를 입력하면 자동으로 호출되는 메소드
+    func textViewDidChange(_ textView: UITextView) {
+        //내용을 최대 15자리까지 읽어 subject 변수에 저장
+        let contents = textView.text as NSString
+        let length = ((contents.length>15) ? 15 : contents.length)
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        // 네비게이션 타이틀에 표시
+        self.navigationItem.title = self.subject
     }
     
     // MARK: 카메라 버튼을 클릭했을 때 호출되는 메소드
@@ -41,8 +75,10 @@ class MemoFormViewController: UIViewController, UIImagePickerControllerDelegate,
         picker.dismiss(animated: false)
     }
     
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.contentsView.delegate = self       //텍스트뷰 델리게이트 정의
 
         // Do any additional setup after loading the view.
     }
