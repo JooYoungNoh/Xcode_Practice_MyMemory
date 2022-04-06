e6aa74c73e678266b07ac8decec0093bd9273b9c
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 01:59:37 +0900

    add : 커스텀 셀 아웃렛 변수 선언 및 연결

--- MyMemory/MyMemory/MemoCell.swift
+++ MyMemory/MyMemory/MemoCell.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class MemoCell: UITableViewCell {
 
-    
+    @IBOutlet var subject: UILabel!
+    @IBOutlet var contents: UILabel!
+    @IBOutlet var regdate: UILabel!
+    @IBOutlet var img: UIImageView!
     
     
     
