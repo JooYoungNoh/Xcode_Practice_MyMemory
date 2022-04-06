11b87400990ec4726e7a1d73db6addc7545c401f
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 02:46:06 +0900

    add : 메모읽기 뷰 코드 작성

--- MyMemory/MyMemory/MemoReadViewController.swift
+++ MyMemory/MyMemory/MemoReadViewController.swift
@@ -15,9 +15,22 @@ class MemoReadViewController: UIViewController {
     @IBOutlet weak var contents: UILabel!
     @IBOutlet weak var img: UIImageView!
     
+    // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //제목, 내용, 이미지 출력
+        self.subject.text = param?.title
+        self.contents.text = param?.contents
+        self.img.image = param?.image
+        
+        //날짜 포맷 변환
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd일 HH:mm분에 작성됨"
+        let dataString = formatter.string(from: (param?.regdate)!)
+        
+        //네비게이션 타이틀에 날짜를 표시
+        self.navigationItem.title = dataString
+        
         // Do any additional setup after loading the view.
     }
     
