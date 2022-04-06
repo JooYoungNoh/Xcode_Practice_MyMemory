479b23d13dcb24fc1462f1dcb0cc88c0fccda08e
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 01:34:24 +0900

    add : 이미지를 선택하기 위한 코드 작성

--- MyMemory/MyMemory/MemoFormViewController.swift
+++ MyMemory/MyMemory/MemoFormViewController.swift
@@ -16,12 +16,29 @@ class MemoFormViewController: UIViewController, UIImagePickerControllerDelegate,
     
     @IBAction func save(_ sender: UIBarButtonItem){
         
+    }
+    
+    // MARK: 카메라 버튼을 클릭했을 때 호출되는 메소드
+    @IBAction func pick(_ sender: UIBarButtonItem){
+        
+        //이미지 피커 인스턴스를 생성
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true     //이미지 편집 허용
+        
+        //이미지 피커 화면을 표시
+        self.present(picker, animated: false)
         
     }
     
-    @IBAction func pick(_ sender: UIBarButtonItem){
+    //사용자가 이미지를 선택하면 자동으로 호출되는 메소드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //선택된 이미지를 미리보기에 출력
+        self.preview.image = info[.editedImage] as? UIImage
         
-        
+        //이미지 피커 컨트롤러를 닫는다
+        picker.dismiss(animated: false)
     }
     
     override func viewDidLoad() {
