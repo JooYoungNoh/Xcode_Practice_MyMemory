aba54b1a79a56f57a708bee2ee274bddae07ba21
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-07 02:29:43 +0900

    add : 목록 뷰 테이블 뷰 화면 구성

--- MyMemory/MyMemory/MemoListViewController.swift
+++ MyMemory/MyMemory/MemoListViewController.swift
@@ -8,16 +8,38 @@
 import UIKit
 
 class MemoListViewController: UIViewController, UITableViewDataSource {
+    //앱 델리게이트 객체의 참조 정보를 읽어온다
+    let appDelgate = UIApplication.shared.delegate as! AppDelegate
     
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: 테이블 구성 메소드들
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼낸다
+        let row = self.appDelgate.memolist[indexPath.row]
         
+        //이미지 속성이 비어 있을 경우 "MemoCell" 아니면 "ImageMemoCell"
+        let Identifier = row.image == nil ? "memoCell" : "imageMemoCell"
+        
+        //재사용 큐로부터 프로토타입 셀의 인스턴스를 전달 받는다
+        let cell = tableView.dequeueReusableCell(withIdentifier: Identifier) as! MemoCell
+        
+        //memoCell의 내용을 구성
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        //Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포맷에 맞게 변경한다
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
+        
+        //cell 객체를 리턴
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return self.appDelgate.memolist.count
     }
     
     func tableview(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
@@ -33,6 +55,7 @@ class MemoListViewController: UIViewController, UITableViewDataSource {
     // MARK: ViewWillAppear
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        self.tableView.reloadData()
         
     }
 
