78aa567f32be873488f86cb2012a9be635cc9559
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 04:11:55 +0900

    add : 네비게이션 타이틀 객체 생성 및 속성 정의

--- CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
+++ CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
@@ -18,7 +18,17 @@ class ViewController: UIViewController {
 
     // MARK: 네비게이션 기능 함수
     func initTitle(){
+        //네비게이션 타이틀 레이블 객체
+        let navigationTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
         
+        //속성 설정
+        navigationTitle.numberOfLines = 2
+        navigationTitle.textAlignment = .center
+        navigationTitle.font = UIFont.systemFont(ofSize: 15)
+        navigationTitle.text = "58개 숙소 \n 1박(1월 10일 ~ 1월 11일)"
+        
+        //네비게이션 타이틀에 입력
+        self.navigationItem.titleView = navigationTitle
     }
 
 }
