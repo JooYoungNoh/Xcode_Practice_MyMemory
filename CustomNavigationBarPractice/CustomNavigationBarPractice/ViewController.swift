3439dc8928de0fc44836b6f1be4d254b980cc0f6
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 04:51:14 +0900

    add : 복합적으로 구성된 네비게이션 바

--- CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
+++ CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.initTitle()
+        //self.initTitle()      //기본 네비게이션
+        self.initTitleNew()     //복합적인 네비게이션
     }
 
     // MARK: 네비게이션 기능 함수
@@ -36,6 +37,40 @@ class ViewController: UIViewController {
         self.navigationController?.navigationBar.barTintColor = color
         self.navigationController?.navigationBar.isTranslucent = false
         
+    }
+    
+    // MARK: 복합적인 네비게이션 바 함수
+    func initTitleNew(){
+        //복합적인 레이아웃을 구현할 컨테이너 뷰
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 36))
+        
+        //상단 레이블 정의
+        let topTitle = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 18))
+        topTitle.numberOfLines = 1
+        topTitle.textAlignment = .center
+        topTitle.font = UIFont.systemFont(ofSize: 15)
+        topTitle.textColor = .white
+        topTitle.text = "58개 숙소"
+        
+        //하단 레이블 정의
+        let subTitle = UILabel(frame: CGRect(x: 0, y: 18, width: 200, height: 18))
+        subTitle.numberOfLines = 1
+        subTitle.textAlignment = .center
+        subTitle.font = UIFont.systemFont(ofSize: 12)
+        subTitle.textColor = .white
+        subTitle.text = "1박(1월 10일 ~ 1월 11일)"
+        
+        //상하단 레이블을 컨테이너 뷰에 추가한다
+        containerView.addSubview(topTitle)
+        containerView.addSubview(subTitle)
+        
+        //네비게이션 타이틀에 컨테이너 뷰를 연결한다
+        self.navigationItem.titleView = containerView
+        
+        //배경 색상 설정
+        let color = UIColor(red: 0.02, green: 0.22, blue: 0.49, alpha: 1.0)
+        self.navigationController?.navigationBar.barTintColor = color
+        self.navigationController?.navigationBar.isTranslucent = false   //반투명(안하면 색 안나옴
     }
 
 }
