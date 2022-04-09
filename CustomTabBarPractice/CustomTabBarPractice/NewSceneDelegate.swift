be97e1a7ab8801a67e6d5f4ee289aaeee084a64c
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 01:07:13 +0900

    add : 스토리보드 없이 탭바를 만들 새로운 SceneDelegate 추가(practice)

---
+++ CustomTabBarPractice/CustomTabBarPractice/NewSceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  NewSceneDelegate.swift
+//  CustomTabBarPractice
+//
+//  Created by 노주영 on 2022/04/10.
+//
+
+import UIKit
+
+class NewSceneDelegate: UIResponder, UIWindowSceneDelegate{
+    var window: UIWindow?
+}
