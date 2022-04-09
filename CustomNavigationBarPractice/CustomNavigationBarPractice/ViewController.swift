c71f7581d5621b9ada84bf0e550267ee23daf665
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 05:05:29 +0900

    add : 타이틀에 텍스트 필드 넣기

--- CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
+++ CustomNavigationBarPractice/CustomNavigationBarPractice/ViewController.swift
@@ -13,9 +13,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        //self.initTitle()      //기본 네비게이션
-        // self.initTitleNew()     //복합적인 네비게이션
-        self.initTitleImage()
+        //self.initTitle()          //기본 네비게이션
+        //self.initTitleNew()       //복합적인 네비게이션
+        //self.initTitleImage()     //타이틀이 이미지인 네비게이션
+        self.initTitleInput()
     }
 
     // MARK: 네비게이션 기능 함수
@@ -80,6 +81,25 @@ class ViewController: UIViewController {
         let imageV = UIImageView(image: image)
         
         self.navigationItem.titleView = imageV
+    }
+    
+    // MARK: 타이틀에 텍스트 필드를 표시하는 함수
+    func initTitleInput(){
+        //텍스트 필드 객체 생성
+        let textfield = UITextField()
+        textfield.frame = CGRect(x: 0, y: 0, width: 300, height: 35)
+        textfield.backgroundColor = .white
+        textfield.font = UIFont.systemFont(ofSize: 13)
+        textfield.autocapitalizationType = .none        //자동 대문자 속성 사용안함
+        textfield.autocorrectionType = .no              //자동 입력 기능 해제
+        textfield.spellCheckingType = .no               //스펠링 체크 기능 해제
+        textfield.keyboardType = .URL                   //URL 입력 전용 키보드
+        textfield.keyboardAppearance = .dark
+        textfield.layer.borderWidth = 0.3               //테두리 경계선 두께
+        textfield.layer.borderColor = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.0).cgColor
+        
+        //타이틀 뷰 속성에 대입
+        self.navigationItem.titleView = textfield
     }
 }
 
