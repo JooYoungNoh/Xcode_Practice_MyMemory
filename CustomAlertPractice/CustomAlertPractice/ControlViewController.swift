13c18d2b02e2bb3b7af28653376c435c674d7ac7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 06:43:07 +0900

    add : 슬라이더 클래스 생성 후 객체 정의

--- CustomAlertPractice/CustomAlertPractice/ControlViewController.swift
+++ CustomAlertPractice/CustomAlertPractice/ControlViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ControlViewController: UIViewController {
-
+    //슬라이더 객체를 정의
+    let slider = UISlider()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //슬라이더의 최소값 / 최대값 설정
+        self.slider.minimumValue = 0
+        self.slider.maximumValue = 100
+        
+        // 슬라이더의 영역과 크기를 정의하고 루트 뷰에 추가
+        self.slider.frame = CGRect(x: 0, y: 0, width: 170, height: 30)
+        self.view.addSubview((self.slider))
+        
+        //뷰 컨트롤러의 콘텐츠 사이즈를 지정
+        self.preferredContentSize = CGSize(width: self.slider.frame.width, height: self.slider.frame.height + 10)
     }
     
 
