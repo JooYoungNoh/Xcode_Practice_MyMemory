8bb421b14f12c2f69328acf4851a24ebb7722e3b
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-10 06:17:19 +0900

    add : 지도 축척 및 핀포인트 추가

--- CustomAlertPractice/CustomAlertPractice/MapAlertViewController.swift
+++ CustomAlertPractice/CustomAlertPractice/MapAlertViewController.swift
@@ -27,6 +27,25 @@ class MapAlertViewController: UIViewController {
         //뷰 컨트롤러에 맵킷 뷰를 추가
         let mapkitView = MKMapView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         contentVC.view = mapkitView
+        contentVC.preferredContentSize.height = 200         //콘텐츠 뷰의 높이 설정
+        
+        //위치 정보를 설정 (위도,경도)
+        let pos = CLLocationCoordinate2D(latitude: 37.514322, longitude: 126.894623)
+        
+        //지도에서 보여줄 넓이(축척), 숫자가 작을수록 좁은 범위를 확대시켜서 보여준다
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        
+        //지도 영역을 정의
+        let region = MKCoordinateRegion(center: pos, span: span)
+        
+        //지도 뷰에 표시
+        mapkitView.region = region
+        mapkitView.regionThatFits(region)
+        
+        //위치를 핀으로 표시
+        let point = MKPointAnnotation()
+        point.coordinate = pos
+        mapkitView.addAnnotation(point)
         
         //뷰 컨트롤러를 알림창의 콘텐츠 뷰 컨트롤러 속성에 등록한다
         alert.setValue(contentVC, forKey: "contentViewController")
