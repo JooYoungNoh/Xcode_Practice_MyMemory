0e366f559d1d4bab3b39aa12727b1b3bcfc3a63f
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 03:13:15 +0900

    initial Commit

---
+++ MyMemory/MyMemory/CSLogButton.swift
@@ -0,0 +1,20 @@
+//
+//  CSLogButton.swift
+//  MyMemory
+//
+//  Created by 노주영 on 2022/04/12.
+//
+
+import UIKit
+
+class CSLogButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
