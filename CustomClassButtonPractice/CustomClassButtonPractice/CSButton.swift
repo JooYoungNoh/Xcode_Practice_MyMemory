343a758448ae487e9cde62f2ed1bd575a5229990
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-11 21:52:18 +0900

    initial Commit

---
+++ CustomClassButtonPractice/CustomClassButtonPractice/CSButton.swift
@@ -0,0 +1,20 @@
+//
+//  CSButton.swift
+//  CustomClassButtonPractice
+//
+//  Created by 노주영 on 2022/04/11.
+//
+
+import UIKit
+
+class CSButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
