0d27fe7bf4c724de49804559f93ed3e720568888
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 00:57:56 +0900

    add : 탭 바 있던 곳에 새로운 뷰 생성

--- CustomClassTabBar/CustomClassTabBar/CSTabBarViewController.swift
+++ CustomClassTabBar/CustomClassTabBar/CSTabBarViewController.swift
@@ -16,8 +16,22 @@ class CSTabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        //기존 탭 바를 숨김 처리
+        self.tabBar.isHidden = true
+        
+        //새로운 탭 바 역할을 할 뷰를 위해 기준 좌표와 크기를 정의
+        let width = self.view.frame.width
+        let height: CGFloat = 50
+        let x: CGFloat = 0
+        let y = self.view.frame.height - height
+        
+        //정의된 값을 이용하여 새로운 뷰의 속성을 설정
+        self.csView.frame = CGRect(x: x, y: y, width: width, height: height)
+        self.csView.backgroundColor = .brown
+        
+        self.view.addSubview(self.csView)
     }
     
 
