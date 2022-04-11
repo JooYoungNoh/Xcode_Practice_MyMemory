ed86ce9a045eb13fce7acfcd86dc8606c61d6945
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-11 21:59:32 +0900

    add : 초기화 메소드 정의 후 버튼 생성

--- CustomClassButtonPractice/CustomClassButtonPractice/CSButton.swift
+++ CustomClassButtonPractice/CustomClassButtonPractice/CSButton.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class CSButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //초기화 메소드 정의
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        
+        //스토리보드 방식으로 버튼을 정의했을 때 적용
+        self.backgroundColor = .green
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+        self.setTitle("버튼", for: .normal)
     }
-    */
-
+    
 }
