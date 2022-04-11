fe9bca8b75450f8e4e3e219f2217e8cb53b0cc66
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 01:35:49 +0900

    initial Commit

---
+++ CustomClassStamperPractice/CustomClassStamperPractice/CSStepper.swift
@@ -0,0 +1,23 @@
+//
+//  CSStepper.swift
+//  CustomClassStamperPractice
+//
+//  Created by 노주영 on 2022/04/12.
+//
+
+import UIKit
+
+public class CSStepper: UIView {
+    //스토리보드에서 호출할 초기화 메소드
+    public required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+    }
+    
+    //코드로 호출할 초기화 메소드
+    public override init(frame: CGRect){
+        super.init(frame: frame)
+        
+    }
+
+}
