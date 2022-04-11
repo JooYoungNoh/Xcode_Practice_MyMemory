c16bfef3c01b0fa2fa18cb87abb1662c1dd71960
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 01:58:36 +0900

    add : 버튼 및 레이블 속성 정의 후 화면에 표시

--- CustomClassStamperPractice/CustomClassStamperPractice/CSStepper.swift
+++ CustomClassStamperPractice/CustomClassStamperPractice/CSStepper.swift
@@ -29,6 +29,36 @@ public class CSStepper: UIView {
     //스테퍼의 기본 속성 정의
     private func setup(){
         
+        let borderWidth: CGFloat = 0.5              //테두리 두께값
+        let borderColor = UIColor.blue.cgColor      //테두리 색상값
+        //좌측 다운 버튼 속성 설정
+        self.leftButton.tag = -1
+        self.leftButton.setTitle("↓", for: .normal)
+        self.leftButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        self.leftButton.layer.borderWidth = borderWidth
+        self.leftButton.layer.borderColor = borderColor
+        
+        //우측 업 버튼 속성 설정
+        self.rightButton.tag = 1
+        self.rightButton.setTitle("↑", for: .normal)
+        self.rightButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        self.rightButton.layer.borderWidth = borderWidth
+        self.rightButton.layer.borderColor = borderColor
+        
+        //중앙 레이블 속성 설정
+        self.centerLabel.text = String(value)
+        self.centerLabel.font = UIFont.systemFont(ofSize: 16)
+        self.centerLabel.textAlignment = .center
+        self.centerLabel.backgroundColor = .cyan
+        self.centerLabel.layer.borderWidth = borderWidth
+        self.centerLabel.layer.borderColor = borderColor
+        
+        //영역별 객체를 서브 뷰로 추가
+        self.addSubview(self.leftButton)
+        self.addSubview(self.rightButton)
+        self.addSubview(self.centerLabel)
     }
 
 }
