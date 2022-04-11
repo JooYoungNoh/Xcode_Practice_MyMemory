46bb0e61618217a7bdfd016ffcbb31e76a704ded
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 00:45:59 +0900

    initial Commit

---
+++ CustomClassTabBar/CustomClassTabBar/CSTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CSTabBarViewController.swift
+//  CustomClassTabBar
+//
+//  Created by 노주영 on 2022/04/12.
+//
+
+import UIKit
+
+class CSTabBarViewController: UITabBarController {
+
+    let csView = UIView()
+    let tabItem1 = UIButton(type: .system)
+    let tabItem2 = UIButton(type: .system)
+    let tabItem3 = UIButton(type: .system)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
