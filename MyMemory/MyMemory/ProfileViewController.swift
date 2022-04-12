408b9c0a8037fc0a31a8e8d3a49c04aa66e4aa1d
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-13 02:49:42 +0900

    add : 프로필 화면 구현 환경 구축

---
+++ MyMemory/MyMemory/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  MyMemory
+//
+//  Created by 노주영 on 2022/04/13.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
