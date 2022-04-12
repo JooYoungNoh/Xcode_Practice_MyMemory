ec8b8314f3236dbf94679c0c0826d74f5bd66d0a
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-12 23:43:44 +0900

    add : 슬라이드 아웃 사이드 바 기능 구현

--- SideBarPractice/SideBarPractice/FrontViewController.swift
+++ SideBarPractice/SideBarPractice/FrontViewController.swift
@@ -16,6 +16,16 @@ class FrontViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //메인 컨트롤러의 참조 정보를 가져온다
+        if let revealVC = self.revealViewController(){
+            //버튼이 클릭될 때 메인 커트롤러에 정의된 revealToggle(_:)을 호출하도록 정의
+            self.sideBarButton.target = revealVC
+            self.sideBarButton.action = #selector(revealVC.revealToggle(_:))
+            
+            //제스처 뷰에 추가한다
+            self.view.addGestureRecognizer(revealVC.panGestureRecognizer())
+        }
     }
     
 
