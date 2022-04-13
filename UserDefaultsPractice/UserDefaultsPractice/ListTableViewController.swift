680059c7f35a78f63393b6c43dc9d10101b4be14
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-14 03:17:06 +0900

    add : 툴 바와 확인 버튼 추가

--- UserDefaultsPractice/UserDefaultsPractice/ListTableViewController.swift
+++ UserDefaultsPractice/UserDefaultsPractice/ListTableViewController.swift
@@ -80,6 +80,29 @@ class ListTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
         //account 텍스트 필드 입력 방식을 가상 키보드 대신 피커 뷰로 설정
         self.account.inputView = picker
 
+        //툴 바 객체 정의
+        let toolBar = UIToolbar()
+        toolBar.frame = CGRect(x: 0, y: 0, width: 0, height: 35)
+        toolBar.barTintColor = .lightGray
+        
+        //액세서리 뷰 영역에 툴 바를 표시
+        self.account.inputAccessoryView = toolBar
+        
+        //툴바에 들어갈 닫기 버튼
+        let done = UIBarButtonItem()
+        done.title = "Done"
+        done.target = self
+        done.action = #selector(pickerDone(_:))
+        
+        //가변 폭 버튼 정의
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        //버튼을 툴 바에 추가
+        toolBar.setItems([flexSpace, done], animated: true)
+    }
+    
+    @objc func pickerDone(_ sender: Any){
+        self.view.endEditing(true)          //입력 뷰를 닫음
     }
 
     // MARK: - Picker view 메소드
@@ -101,8 +124,6 @@ class ListTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
         let account1 = self.accountList[row]
         self.account.text = account1
         
-        //입력 뷰를 닫음
-        self.view.endEditing(true)
     }
     
     // MARK: - Table view data source
