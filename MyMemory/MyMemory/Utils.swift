76f8d1ee3bbd91c34f6f425e262995c05db7a67c
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-15 03:11:22 +0900

    add : 스토리보드 분리

---
+++ MyMemory/MyMemory/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  MyMemory
+//
+//  Created by 노주영 on 2022/04/15.
+//
+
+import UIKit
+extension UIViewController {
+    var tutorialSB = UIStoryboard {
+        return UIStoryboard(name: "Tutorial", bundle: Bundle.main)
+    }
+    
+    func instanceTutorialVC(name: String) -> UIViewController? {
+        return self.tutorialSB.instantiateViewController(withIdentifier: name)
+    }
+    
+}
