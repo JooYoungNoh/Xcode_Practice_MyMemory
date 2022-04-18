78ce47a670a850589923361840f4ca90e42b1ba7
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-19 04:32:17 +0900

    add: 데이터베이스 파일의 경로 읽어오는 코드

--- SQLite3Practice/SQLite3Practice/ViewController.swift
+++ SQLite3Practice/SQLite3Practice/ViewController.swift
@@ -11,7 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        var db: OpaquePointer? = nil                //SQLite 연결 정보를 담을 객체
+        var stmt: OpaquePointer? = nil              //컴파일된 SQL을 담을 객체
+        
+        //앱 내 문서 디렉터리 경로에서 SQLite DB 파일을 찾는다
+        let fileMgr = FileManager()                 //파일 매니저 객체를 생성
+        let docPathURL = fileMgr.urls(for: .desktopDirectory, in: .userDomainMask).first!                                      //앱 내의 문서 디렉터리 경로를 찾고 이를 URL객체로 생성
+        let dbPath = docPathURL.appendingPathComponent("db.sqlite")?.path                                                       //URL객체에 "db.sqlite"파일 경로를 추가 후 경로 생성
+        
     }
 
 
