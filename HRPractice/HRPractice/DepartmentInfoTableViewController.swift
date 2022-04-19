bce0cb4f192fd668d84206b6e8cf882679cf4bc4
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-20 06:42:30 +0900

    add : 부서 상세정보 화면 구현

--- HRPractice/HRPractice/DepartmentInfoTableViewController.swift
+++ HRPractice/HRPractice/DepartmentInfoTableViewController.swift
@@ -27,13 +27,90 @@ class DepartmentInfoTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        //헤더에 들어갈 레이블 객체 정의
+        let textHeader = UILabel(frame: CGRect(x: 35, y: 5, width: 200, height: 30))
+        textHeader.font = UIFont.systemFont(ofSize: 15, weight: UIFont.Weight(rawValue: 2.5))
+        textHeader.textColor = UIColor(red: 0.03, green: 0.28, blue: 0.71, alpha: 1.0)
+        
+        //헤더에 들어갈 이미지 뷰 객체 정의
+        let icon = UIImageView()
+        icon.frame = CGRect(x: 10, y: 10, width: 20, height: 20)
+        
+        //섹션에 따라 타이틀과 이미지를 다르게 설정
+        if section == 0 {
+            textHeader.text = "부서 정보"
+            icon.image = UIImage(imageLiteralResourceName: "depart")
+        } else {
+            textHeader.text = "소속 사원"
+            icon.image = UIImage(imageLiteralResourceName: "employee")
+        }
+        
+        //레이블과 이미지 뷰를 담을 컨테이너용 뷰 객체 정의
+        let v = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        v.backgroundColor = UIColor(red: 0.93, green: 0.96, blue: 0.99, alpha: 1.0)
+        
+        //뷰에 레이블과 이미지 추가
+        v.addSubview(textHeader)
+        v.addSubview(icon)
+        
+        return v
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return 3
+        } else {
+            return self.empList.count
+        }
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {             //부서 정보
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DEPART_CELL")
+            
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 13)
+            cell?.detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
+            
+            switch indexPath.row{
+            case 0:
+                cell?.textLabel?.text = "부서 코드"
+                cell?.detailTextLabel?.text = "\(self.departInfo.departCd)"
+            case 1:
+                cell?.textLabel?.text = "부서명"
+                cell?.detailTextLabel?.text = self.departInfo.departTitle
+            case 2:
+                cell?.textLabel?.text = "부서 주소"
+                cell?.detailTextLabel?.text = self.departInfo.departAddr
+            default :
+                ()
+            }
+            return cell!
+            
+        } else {                                //소속 사원
+            let row = self.empList[indexPath.row]
+            
+            //테이블 뷰 셀 설정
+            let cell = tableView.dequeueReusableCell(withIdentifier: "EMP_CELL")
+            cell?.textLabel?.text = "\(row.empName) (입사일: \(row.joinDate)"
+            cell?.textLabel?.font = UIFont.systemFont(ofSize: 12)
+            
+            //재직 상태를 나타내는 세그먼트 컨트롤
+            let state = UISegmentedControl(items: ["재직중", "휴직", "퇴사"])
+            state.frame.origin.x = self.view.frame.width - state.frame.width - 10
+            state.frame.origin.y = 10
+            state.selectedSegmentIndex = row.stateCd.rawValue   //DB로 저장된 값으로 지정
+            
+            cell?.contentView.addSubview(state)
+            
+            return cell!
+        }
+    }
 }
