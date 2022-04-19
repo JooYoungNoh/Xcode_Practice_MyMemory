fd58258c05af33e49f40e69388194d8e855bd1f9
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-20 03:39:16 +0900

    add : EmpStateType 정의
    재직상태를 나타낼 열거형

--- HRPractice/HRPractice/EmployeeDAO.swift
+++ HRPractice/HRPractice/EmployeeDAO.swift
@@ -6,7 +6,18 @@
 //
 
 enum EmpStateType: Int {
+    case ING = 0, STOP, OUT     // 재직중, 휴직, 퇴사
     
+    func desc() -> String{
+        switch self {
+        case .ING:
+            return "재직중"
+        case .STOP:
+            return "휴직"
+        case .OUT:
+            return "퇴사"
+        }
+    }
     
 }
 
