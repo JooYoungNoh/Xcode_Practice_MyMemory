75fdbeacfb5e0327c19f9d4f65edf7c13b5f77bc
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-20 02:43:35 +0900

    add : FMDatabase 객체 생성 및 프로퍼티에 저장

--- HRPractice/HRPractice/DepartmentDAO.swift
+++ HRPractice/HRPractice/DepartmentDAO.swift
@@ -5,9 +5,29 @@
 //  Created by 노주영 on 2022/04/20.
 //
 
-import Foundation
-
-class DepartmetDAO{
+class DepartmentDAO {
+    //부서 정보를 담을 튜플 타입 정의
+    typealias DepartRecord = (Int, String, String)
+    
+    //SQLite 연결 및 초기화
+    lazy var fmdb: FMDatabase! = {
+        //파일 매니저 객체 생성
+        let fileMgr = FileManager.default
+        
+        //샌드박스 내 문서 디렉터리에서 데이터베이스 파일 경로를 확인
+        let docPath = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first
+        let dbPath = docPath!.appendingPathComponent("hr.sqlite").path
+        
+        //샌드박스 경로에 파일이 없다면 메인 번들에 만들어 둔 hr.sqlite를 가져와 복사
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "hr", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        
+        //준비된 데이터베이스 파일을 바탕으로 FMDatabase 객체를 생성
+        let db = FMDatabase(path: dbPath)
+        return db
+    }()
     
     
 }
