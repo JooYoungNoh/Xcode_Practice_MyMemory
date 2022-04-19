b295e4df04e8b383028caaf0336a878b301450c1
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-20 03:17:18 +0900

    add : 사원 정보 DAO 파일 생성

---
+++ HRPractice/HRPractice/EmployeeDAO.swift
@@ -0,0 +1,19 @@
+//
+//  EmployeeDAO.swift
+//  HRPractice
+//
+//  Created by 노주영 on 2022/04/20.
+//
+
+enum EmpStateType: Int {
+    
+    
+}
+
+struct EmployeeV0{
+    
+}
+
+class EmployeeDAO {
+    
+}
