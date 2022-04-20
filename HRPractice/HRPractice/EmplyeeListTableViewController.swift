c43fe7ab87b6dc216430fb998d4f67f785fe0920
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-21 00:30:57 +0900

    add : 새로고침 기능 커스텀마이징

--- HRPractice/HRPractice/EmplyeeListTableViewController.swift
+++ HRPractice/HRPractice/EmplyeeListTableViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class EmplyeeListTableViewController: UITableViewController {
-    //데이터 소스를 저장할 변수
-    var empList: [EmployeeVO]!
-    //SQLite 처리를 담당할 DAO 클래스
-    var empDAO = EmployeeDAO()
+    var empList: [EmployeeVO]!                 //데이터 소스를 저장할 변수
+    var empDAO = EmployeeDAO()                 //SQLite 처리를 담당할 DAO 클래스
+    var loadingImg: UIImageView!               //새로고침 컨트롤에 들어갈 이미지 뷰
+    var bgCircle: UIView!                      //임계점에 도달했을 때 나타날 배경 뷰, 노란원 상태
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +20,22 @@ class EmplyeeListTableViewController: UITableViewController {
         
         //당겨서 새로고침 기능
         self.refreshControl = UIRefreshControl()
-        self.refreshControl?.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+       // self.refreshControl?.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
         self.refreshControl?.addTarget(self, action: #selector(pullToRefresh(_:)), for: .valueChanged)
+        
+        self.loadingImg = UIImageView(image: UIImage(named: "refresh"))
+        self.loadingImg.center.x = (self.refreshControl?.frame.width)! / 2
+        self.refreshControl?.tintColor = .clear
+        self.refreshControl?.addSubview(self.loadingImg)
+        
+        //배경 뷰 초기화 및 노란 원 형태를 위한 속성 설정
+        self.bgCircle = UIView()
+        self.bgCircle.backgroundColor = UIColor.yellow
+        self.bgCircle.center.x = (self.refreshControl?.frame.width)! / 2
+        
+        //배경 뷰를 refreshControl 객체에 추가하고, 로딩 이미지를 맨 위로 올림
+        self.refreshControl?.addSubview(self.bgCircle)
+        self.refreshControl?.bringSubviewToFront(self.loadingImg)
     }
 
     // MARK: - Table view data source
@@ -122,6 +136,16 @@ class EmplyeeListTableViewController: UITableViewController {
         self.empList = self.empDAO.find()
         self.tableView.reloadData()
         
+        //노란 원이 로딩 이미지 중심으로 커지는 애니메이션
+        let distance = max(0, 0, -(self.refreshControl?.frame.origin.y)!)
+        UIView.animate(withDuration: 0.5){
+            self.bgCircle.frame.size.width = 80
+            self.bgCircle.frame.size.height = 80
+            self.bgCircle.center.x = (self.refreshControl?.frame.width)! / 2
+            self.bgCircle.center.y = distance / 2
+            self.bgCircle.layer.cornerRadius = (self.bgCircle?.frame.size.width)! / 2
+        }
+        
         //당겨서 새로고침 기능 종료
         self.refreshControl?.endRefreshing()
     }
@@ -137,5 +161,28 @@ class EmplyeeListTableViewController: UITableViewController {
         navTitle.text = "사원 목록 \n" + "총 \(self.empList.count) 명"
         
         self.navigationItem.titleView = navTitle
+    }
+    
+    //새로고침 스크롤이 발생할 때 마다 처리할 내용
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //당긴 거리
+        let distance = max(0, 0, -(self.refreshControl?.frame.origin.y)!)
+        
+        //center.y 좌표를 당긴 거리만큼 수정
+        self.loadingImg.center.y = distance / 2
+        
+        //당긴 거리를 회전 각도로 반환하여 로딩 이미지에 설정
+        let ts = CGAffineTransform(rotationAngle: CGFloat(distance / 20))
+        self.loadingImg.transform = ts
+        
+        //배경 뷰의 중심 좌표 설정
+        self.bgCircle.center.y = distance / 2
+    }
+    
+    //스크롤 뷰의 드래그가 끝났을 때 호출되는 메소드
+    override func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        //노란원을 다시 초기화
+        self.bgCircle.frame.size.width = 0
+        self.bgCircle.frame.size.height = 0
     }
 }
