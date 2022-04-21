0fd9c97116f82815f33113e41af56eb81d1e872f
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-22 04:24:14 +0900

    add : 코어데이터를 사용하기 위한 코드 작성

--- MyMemory/MyMemory/MemoData.swift
+++ MyMemory/MyMemory/MemoData.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import CoreData
 
 class MemoData{
     
-    var memoID: Int?        //아이디 식별값
-    var title: String?      //메모 제목
-    var contents: String?   //메모 내용
-    var image: UIImage?     //이미지
-    var regdate: Date?      //작성일
+    var memoID: Int?                    //아이디 식별값
+    var title: String?                  //메모 제목
+    var contents: String?               //메모 내용
+    var image: UIImage?                 //이미지
+    var regdate: Date?                  //작성일
+    var objectID: NSManagedObject?      //원본 MemoMO 객체를 참조하기 위한 속성
 }
