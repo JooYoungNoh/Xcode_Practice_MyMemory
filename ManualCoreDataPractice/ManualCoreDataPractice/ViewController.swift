80634efcf6901a96f6404f5c32b4b4b50f96a47d
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-22 04:10:19 +0900

    initial Commit

---
+++ ManualCoreDataPractice/ManualCoreDataPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManualCoreDataPractice
+//
+//  Created by 노주영 on 2022/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
