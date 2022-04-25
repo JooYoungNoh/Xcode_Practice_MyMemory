6fb862cb4b4e6ab3c23d360c36af1d5bd6d358cb
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-26 03:13:45 +0900

    add : 객체 생성 및 연결

--- MyMemory/MyMemory/JoinVC.swift
+++ MyMemory/MyMemory/JoinVC.swift
@@ -9,10 +9,17 @@ import UIKit
 import Alamofire
 
 class JoinVC: UIViewController {
-
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    //MARK: 아울렛 메소드
+    @IBAction func submit(_ sender: Any){
+        
+    }
 }
