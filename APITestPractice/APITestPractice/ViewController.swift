127e19e2e3d04beae0dc79d7e4687a48df45e2a4
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-25 23:57:26 +0900

    add : JSON 형식으로 API 호출

--- APITestPractice/APITestPractice/ViewController.swift
+++ APITestPractice/APITestPractice/ViewController.swift
@@ -93,7 +93,60 @@ class ViewController: UIViewController {
     }
 
     @IBAction func json(_ sender: UIButton){
+        //전송할 값 준비
+        let userId = (self.userId.text)!
+        let name = (self.name.text)!
+        let param = ["userId" : userId, "name" : name]
+        let paramData = try! JSONSerialization.data(withJSONObject: param, options: [])
+    
+        //URL 객체 정의
+        let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/echoJSON")
+    
+        //URLRequest 객체 정의 및 요청 내용 담기
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = paramData
         
+        //HTTP 메시지가 포함될 헤더 설정
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(String(paramData.count), forHTTPHeaderField: "Content-Length")
+        
+        //URLSession 객체를 통해 전송 및 응답값 처리 로직 작성
+        let task = URLSession.shared.dataTask(with: request){ (data, response, error) in
+            //서버가 응답이 없거나 통신이 실패 했을 때
+            if let e = error{
+                NSLog("An Error has occurred : \(e.localizedDescription)")
+                return
+            }
+            //응답 처리 로직
+            DispatchQueue.main.async {
+                do{
+                    let object = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary
+                    guard let jsonObject = object else { return }
+                    
+                    //JSON 결과값 추출
+                    let result = jsonObject["result"] as? String
+                    let timestamp = jsonObject["timestamp"] as? String
+                    let userId = jsonObject["userId"] as? String
+                    let name = jsonObject["name"] as? String
+                    
+                    //결과가 성공일 때만 텍스트 뷰에 출력
+                    if result == "SUCCESS" {
+                        self.responseView.text =
+                        "아이디 : \(userId!)" + "\n" +
+                        "이름 : \(name!)" + "\n" +
+                        "응답결과 : \(result!)" + "\n" +
+                        "응답시간 : \(timestamp!)" + "\n" +
+                        "요청방식 : application/json"
+                    }
+                } catch let error as NSError{
+                    print("An error has occuredd while parsing JSONObject : \(error.localizedDescription)")
+                }
+            }
+            
+        }
+        // POST 전송
+        task.resume()
     }
 
 }
