ef98064e4a1aa1835f60dc305bece04ea6d1b527
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-26 03:10:37 +0900

    add : 신규 계정 등록 화면 구성 및 뷰 컨트롤러와 연결

---
+++ MyMemory/MyMemory/JoinVC.swift
@@ -0,0 +1,18 @@
+//
+//  JoinVC.swift
+//  MyMemory
+//
+//  Created by 노주영 on 2022/04/26.
+//
+
+import UIKit
+import Alamofire
+
+class JoinVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
