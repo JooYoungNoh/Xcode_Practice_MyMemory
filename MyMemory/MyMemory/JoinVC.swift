a199d8f2dafb0929e3406fb8ed39a7146ddf0eeb
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-26 03:26:04 +0900

    add : 테이블 뷰 셀 기본 구성

--- MyMemory/MyMemory/JoinVC.swift
+++ MyMemory/MyMemory/JoinVC.swift
@@ -24,11 +24,43 @@ class JoinVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //MARK: 테이블 뷰 메소드
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        //각 테이블 뷰 셀 모두에 공통으로 적용될 프레임 객체
+        let tframe = CGRect(x: 20, y: 0, width: cell.bounds.width - 20, height: 37)
+        switch indexPath.row{
+        case 0 :
+            self.fieldAccount = UITextField(frame: tframe)
+            self.fieldAccount.placeholder = "계정(이메일)"
+            self.fieldAccount.borderStyle = .none
+            self.fieldAccount.autocapitalizationType = .none
+            self.fieldAccount.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldAccount)
+        case 1 :
+            self.fieldPassword = UITextField(frame: tframe)
+            self.fieldPassword.placeholder = "비밀번호"
+            self.fieldPassword.borderStyle = .none
+            self.fieldPassword.isSecureTextEntry = true
+            self.fieldPassword.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldPassword)
+        case 2 :
+            self.fieldName = UITextField(frame: tframe)
+            self.fieldName.placeholder = "이름"
+            self.fieldName.borderStyle = .none
+            self.fieldName.font = UIFont.systemFont(ofSize: 14)
+            cell.addSubview(self.fieldName)
+        default :
+            ()
+        }
+        return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
     }
 
     //MARK: 아울렛 메소드
