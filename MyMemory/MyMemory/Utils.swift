124430c3609008aafc14a8d30cc200455b6e84f0
Noh Joo Young <96951827+JooYoungNoh@users.noreply.github.com>
2022-04-27 01:05:04 +0900

    add : 키 체인 값을 저장하는 메소드

--- MyMemory/MyMemory/Utils.swift
+++ MyMemory/MyMemory/Utils.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import Security
+import Alamofire
+
 extension UIViewController {
     var tutorialSB: UIStoryboard {
         return UIStoryboard(name: "Tutorial", bundle: Bundle.main)
@@ -26,6 +29,28 @@ extension UIViewController {
             self.present(alert, animated: false)
         }
         
+    }
+    
+}
+
+//인증 토큰
+class TokenUtils{
+    //키 체인에 값을 저장하는 메소드
+    func save(_ service: String, account: String, value: String){
+        let keyChainQuery: NSDictionary = [
+            kSecClass : kSecClassGenericPassword,
+            kSecAttrService : service,
+            kSecAttrAccount : account,
+            kSecValueData : value.data(using: .utf8, allowLossyConversion:  false)!
+        ]
+        
+        //현재 저장되어 있는 값 객체
+        SecItemDelete(keyChainQuery)
+        
+        //새로운 키 체인 아이템 등록
+        let status: OSStatus = SecItemAdd(keyChainQuery, nil)
+        assert(status == noErr, "토큰 값 저장에 실패했습니다.")
+        NSLog("status=\(status)")
     }
     
 }
